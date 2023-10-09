39a99a8bb584330647fc141ae23050ad6f129f0a
NatLane2002 <nathaniel.lane@keyin.com>
2023-10-09 15:38:51 -0230

    task 3 - finished with event emitter

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ global.DEBUG = true;
 
 const http = require("http");
 const fs = require("fs"); // Require the 'fs' module for file operations
+const EventEmitter = require('events');
+
+// Create a new event emitter
+const myEmitter = new EventEmitter();
 
 const server = http.createServer(function (req, res) {
     switch (req.url) {
@@ -46,8 +50,12 @@ function serveHTML(fileName, res) {
     fs.readFile(fileName, "utf8", function (err, data) {
         if (err) {
             console.error(`Error reading file ${fileName}: ${err.message}`);
-            serve404(res);
+            serveErrorPage(res);
         } else {
+            if (fileName != "views/index.html") {
+                myEmitter.emit('notHomeRouteAccessed', `The route ${fileName} was accessed - this is not the home route.`);
+            }
+            myEmitter.emit('httpStatus', 200);
             res.writeHead(200, { "Content-Type": "text/html" });
             res.write(data);
             res.end();
@@ -66,6 +74,7 @@ function serveErrorPage(res) {
             res.write("<p>The page you are looking for does not exist.</p>");
             res.end();
         } else {
+            myEmitter.emit('httpStatus', 404);
             res.writeHead(404, { "Content-Type": "text/html" });
             res.write(data);
             res.end();
@@ -73,5 +82,18 @@ function serveErrorPage(res) {
     });
 }
 
+// Event emitter for HTTP status codes
+// This event is emitted to capture and log HTTP status codes generated during server responses. It provides visibility into the status of HTTP requests, allowing you to monitor and record the outcome of each request. This is useful for debugging and troubleshooting.
+myEmitter.on('httpStatus', (statusCode) => {
+    console.log(`HTTP Status code: ${statusCode}`);
+});
+
+// Event emitter for routes that are not the home route
+// This event is emitted when a client accesses a route that is not the home route. It provides visibility into the routes that are being accessed. This is useful for debugging and troubleshooting.
+myEmitter.on('notHomeRouteAccessed', (message) => {
+    console.warn(`Message: ${message}`);
+});
+
+// Start the server
 server.listen(3000);
 console.log("Listening on port 3000...");
